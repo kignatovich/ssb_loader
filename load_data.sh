d682a427567eeebb0432e9932fc096b9832b55d4
Kiryl Ihnatovich <110161538+kignatovich@users.noreply.github.com>
2025-01-11 09:30:31 +0300

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 
-# Задержка, чтобы ClickHouse сервер успел запуститься
-sleep 10
+#Установка фактора масштаба
+ScaleFactor=10
+
+echo "Загрузка информации о личной жизни Романа Морозова"
+
+# Ждем, пока ClickHouse сервер станет доступным
+until clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query="SELECT 1" &>/dev/null; do
+  echo "Ждем, пока ClickHouse станет доступным..."
+  sleep 1
+done
+
+# Проверяем наличие всех необходимых таблиц
+TABLES_EXIST=$(clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query="
+    SELECT COUNT() 
+    FROM system.tables 
+    WHERE database = 'default' 
+    AND name IN ('customer', 'date', 'lineorder', 'supplier', 'part');")
+
+# Если все 5 таблиц существуют, завершаем работу
+if [ "$TABLES_EXIST" -eq 5 ]; then
+  echo "Все таблицы уже существуют. Завершаем работу."
+  exit 0
+fi
+
+# Создаем таблицы, если их нет
+echo "Создаем таблицы..."
+clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --multiquery < /docker-entrypoint-initdb.d/create_tables.sql
 
 # Переходим в директорию генерации данных
 cd /opt/ssb-dbgen
 
-# Генерируем данные с масштабным фактором 1000
-./dbgen -s 1000 -T c
-./dbgen -s 1000 -T l
-./dbgen -s 1000 -T p
-./dbgen -s 1000 -T s
-./dbgen -s 1000 -T d
+# Генерируем данные и загружаем их в таблицы
 
-# Вот тут нужно поиграться с логином паролем
-# Создаем таблицы в ClickHouse
-clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --multiquery < /docker-entrypoint-initdb.d/create_tables.sql
+echo "Генерация данных и загрузка в таблицы customer.tbl"
+./dbgen -s $ScaleFactor -T c && clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO customer FORMAT CSV" < customer.tbl
 
-# Загружаем данные 
-clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO customer FORMAT CSV" < customer.tbl
-clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO part FORMAT CSV" < part.tbl
-clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO supplier FORMAT CSV" < supplier.tbl
-clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO lineorder FORMAT CSV" < lineorder.tbl
-clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO date FORMAT CSV" < date.tbl
+echo "Генерация данных и загрузка в таблицы part.tbl"
+./dbgen -s $ScaleFactor -T p && clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO part FORMAT CSV" < part.tbl
+
+echo "Генерация данных и загрузка в таблицы supplier.tbl"
+./dbgen -s $ScaleFactor -T s && clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO supplier FORMAT CSV" < supplier.tbl
+
+echo "Генерация данных и загрузка в таблицы lineorder.tbl"
+./dbgen -s $ScaleFactor -T l && clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO lineorder FORMAT CSV" < lineorder.tbl
+
+echo "Генерация данных и загрузка в таблицы date.tbl"
+./dbgen -s $ScaleFactor -T d && clickhouse-client --host=clickhouse --port=9000 --user=admin --password=admin_password --query "INSERT INTO date FORMAT CSV" < date.tbl
+
+echo "Инициализация базы данных завершена."
